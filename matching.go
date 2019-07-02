883bc29ec1854ec58e73a5fef01bd9f4e73f186e
Konstantin Slavnov <konstantin@sourced.tech>
2019-07-02 12:40:00 +0300

    Add basic matching
    Signed-off-by: Konstantin Slavnov <konstantin@sourced.tech>

--- matching.go
+++ matching.go
@@ -1,6 +1,20 @@
 package idmatch
 
-import "github.com/src-d/eee-identity-matching/external"
+import (
+	"github.com/src-d/eee-identity-matching/external"
+	simplegraph "gonum.org/v1/gonum/graph"
+	"gonum.org/v1/gonum/graph/simple"
+	"gonum.org/v1/gonum/graph/topo"
+)
+
+type node struct {
+	Value *Person
+	id    int64
+}
+
+func (g node) ID() int64 {
+	return g.id
+}
 
 // ReducePeople merges the identities together by following the fixed set of rules.
 // 1. Run the external matching, if available.
@@ -10,5 +24,51 @@ import "github.com/src-d/eee-identity-matching/external"
 // The heuristics are:
 // TODO(vmarkovtsev): describe the current approach
 func ReducePeople(people People, ext external.Matcher) error {
+	// TODO(zurk): implement external matching
+
+	peopleGraph := simple.NewUndirectedGraph()
+	for index, person := range people {
+		peopleGraph.AddNode(node{person, int64(index)})
+	}
+
+	// Add edges by the same unpopular email
+	email2id := make(map[string]simplegraph.Node)
+	for index, person := range people {
+		for _, email := range person.Emails {
+			if isPopularEmail(email) {
+				continue
+			}
+			if val, ok := email2id[email]; ok {
+				peopleGraph.SetEdge(peopleGraph.NewEdge(val, peopleGraph.Node(int64(index))))
+			} else {
+				email2id[email] = peopleGraph.Node(int64(index))
+			}
+		}
+	}
+
+	// Add edges by the same unpopular name
+	name2id := make(map[string]simplegraph.Node)
+	for index, person := range people {
+		for _, name := range person.Names {
+			if isPopularName(name) {
+				continue
+			}
+			if val, ok := name2id[name]; ok {
+				peopleGraph.SetEdge(peopleGraph.NewEdge(val, peopleGraph.Node(int64(index))))
+			} else {
+				name2id[name] = peopleGraph.Node(int64(index))
+			}
+		}
+	}
+
+	for _, component := range topo.ConnectedComponents(peopleGraph) {
+		var toMerge []uint64
+		for _, node := range component {
+			toMerge = append(toMerge, uint64(node.ID()))
+		}
+		people.Merge(toMerge...)
+	}
+
 	return nil
+
 }
---
+++ matching_test.go
@@ -0,0 +1,29 @@
+package idmatch
+
+import (
+	"github.com/src-d/eee-identity-matching/external"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestReducePeople(t *testing.T) {
+	var people = People{
+		1: {ID: "_1", Names: []string{"Bob 1"}, Emails: []string{"Bob@google.com"}},
+		2: {ID: "_2", Names: []string{"Bob 2"}, Emails: []string{"Bob@google.com"}},
+		3: {ID: "_3", Names: []string{"Alice"}, Emails: []string{"alice@google.com"}},
+		4: {ID: "_4", Names: []string{"Bob"}, Emails: []string{"Bob@google.com"}},
+		5: {ID: "_5", Names: []string{"popular"}, Emails: []string{"Bob@google.com"}},
+		6: {ID: "_6", Names: []string{"popular"}, Emails: []string{"email@google.com"}},
+		7: {ID: "_7", Names: []string{"Alice"}, Emails: []string{"popular@google.com"}},
+	}
+
+	var reducedPeople = People{
+		1: {ID: "_1", Names: []string{"Bob", "Bob 1", "Bob 2", "popular"}, Emails: []string{"Bob@google.com"}},
+		3: {ID: "_3", Names: []string{"Alice"}, Emails: []string{"alice@google.com", "popular@google.com"}},
+		6: {ID: "_6", Names: []string{"popular"}, Emails: []string{"email@google.com"}},
+	}
+
+	err := ReducePeople(people, external.BitBucketMatcher{})
+	require.Equal(t, err, nil)
+	require.Equal(t, people, reducedPeople)
+}
